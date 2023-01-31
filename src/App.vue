06c8146b2eda7d68f9f8f46611cc9abd6a5b8900
Abdullokh <abdullooffical@gmail.com>
2023-02-01 04:54:47 +0500

    added (components) (start)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- container boshlandi -->
+    <div class="container-fluid">
+
+        <HeaderRow/>
+
+        <!-- main qatori boshlandi -->
+        <div class="row mt-4">
+
+            <CategoryCol/>
+
+            <!-- kontent boshlandi -->
+            <div class="col-12 col-md-10 mt-4 mt-md-0">
+
+               <BooksRow/>
+                <PaginationRow/>
+
+            </div>
+            <!-- kontent tugadi -->
+
+        </div>
+        <!-- main qatori tugadi -->
+
+        <FooterRow/>
+
+    </div>
+    <!-- container tugadi -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderRow from "@/components/HeaderRow.vue";
+import CategoryCol from "@/components/CategoryCol.vue";
+import FooterRow from "@/components/FooterRow.vue";
+import BooksRow from "@/components/BooksRow.vue";
+import PaginationRow from "@/components/PaginationRow.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {PaginationRow, BooksRow, FooterRow, CategoryCol, HeaderRow}
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
