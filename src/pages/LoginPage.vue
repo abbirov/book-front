699c232c05091f18792c5df290de9a35c5b20859
Abdullokh <abdullooffical@gmail.com>
2023-02-01 09:05:41 +0500

    added (user.js, store.js)

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,12 +1,12 @@
 <template>
-    <form>
+    <form @submit.prevent="auth">
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input v-model="form.email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <input v-model="form.password" type="password" class="form-control" id="exampleInputPassword1">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
@@ -14,8 +14,25 @@
 </template>
 
 <script>
+import {mapActions} from "vuex";
+
 export default {
-    name: "LoginPage"
+    name: "LoginPage",
+    data(){
+        return {
+            form: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        ...mapActions(['fetchToken']),
+        auth(){
+            this.fetchToken(this.form)
+        }
+    }
+
 }
 </script>
 
---
+++ src/plugins/vuex/user.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export default {
+    actions: {
+        fetchToken(context, data){
+            return new Promise((resolve, reject) => {
+                axios.post('http://localhost:2205/api/users/auth', data)
+                    .then((response) => {
+                        console.log('Token olindi')
+
+                        context.commit('updateToken', response.data.token)
+                        resolve()
+                    })
+                    .catch(() => {
+                        console.log('Token olishda xatolik')
+                        reject()
+                    })
+            })
+        }
+    },
+    mutations: {
+        updateToken(state, token){
+            localStorage.setItem('token', token)
+            state.token = token
+        }
+    },
+    state: {
+        token: localStorage.getItem('token')
+    },
+    getters: {
+        getToken(state){
+            return state.token
+        }
+    }
+}
